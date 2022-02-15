b87fc1b1cda069be5ee82ffff67f0d86f435d00f
Theo JOLY <theo.joly@ens.uvsq.fr>
2022-02-15 11:42:15 +0100

    maj

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -28,14 +28,10 @@ root.title("Tas de sable")
 
 Button1.config(command=config_aleatoire())
 Button1.grid(row=0, column=0)
-label.config()
-import tkinter as tk
+label.config(text="test")
+label.grid(row=0,column=1)
 
-racine = tk.Tk() # Création de la fenêtre racine
-racine.title("Un premier exemple") # ajoute un titre
-label = tk.Label(racine, text="Un texte dans ma fenêtre", font=("helvetica", "20")) # création du widget
-label.grid() # positionnement du widget
-racine.mainloop() # Lancement de la boucle principale
+
 # laisser à la fin
 root.mainloop()
 
@@ -44,8 +40,12 @@ root.mainloop()
 
 # debut de code
 
-
-
+""" exemple:
+racine = tk.Tk() # Création de la fenêtre racine
+racine.title("Un premier exemple") # ajoute un titre
+label = tk.Label(racine, text="Un texte dans ma fenêtre", font=("helvetica", "20")) # création du widget
+label.grid() # positionnement du widget
+racine.mainloop() # Lancement de la boucle principale"""
 
 racine= tk.Tk()
 racine.title("Tas de sable")

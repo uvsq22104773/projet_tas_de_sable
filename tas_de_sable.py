eea07b89ba1444eea20531151cd24a8b65d110b0
Ouail <ouail.djebrouni@ens.uvsq.fr>
2022-02-15 10:30:32 +0100

    MAJ2

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,7 +1,7 @@
 #########################################
 # groupe BITD 1
 # Théo JOLY
-# Djebrouni Ouail1
+# Djebrouni Ouail
 # Ledien Nils
 # Matveev Erik
 # https://github.com/uvsq-info/l1-python

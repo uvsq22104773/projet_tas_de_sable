63af0316802c10ee4c976bb372ce1a0e025f4c8e
Nils LEDIEN <nils.ledien@ens.uvsq.fr>
2022-02-15 11:40:16 +0100

    jsp

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -28,12 +28,27 @@ root.title("Tas de sable")
 
 Button1.config(command=config_aleatoire())
 Button1.grid(row=0, column=0)
-label.config(text="test")
-label.grid(row=0,column=1)
+label.config()
+import tkinter as tk
 
-
+racine = tk.Tk() # Création de la fenêtre racine
+racine.title("Un premier exemple") # ajoute un titre
+label = tk.Label(racine, text="Un texte dans ma fenêtre", font=("helvetica", "20")) # création du widget
+label.grid() # positionnement du widget
+racine.mainloop() # Lancement de la boucle principale
 # laisser à la fin
 root.mainloop()
 
 
 ############################################################################################################
+
+# debut de code
+
+
+
+
+racine= tk.Tk()
+racine.title("Tas de sable")
+canvas = tk.Canvas(racine, bg="whit", height=500, width=500)
+label = tk.Label(racine, text="tasser le sable", font=("helvetica", "20"))
+
